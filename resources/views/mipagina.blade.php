109ba7d25491c304195d7a7afa4676a806e116de
marcelo <fleitascampeloml@gmail.com>
2018-05-21 17:28:43 -0300

    Subiendo cambios

---
+++ resources/views/mipagina.blade.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title> primer pagina</title>
+    </head>
+        <body>
+        <p> <h1> epa epa<p/>
+        </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/prueba', function () {
+    return view('mipagina');
+});
+
+Route::get('/prueba2', function () {
+    return view('mipagina');
 });

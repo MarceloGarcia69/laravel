664b046e189c959bc1f84241f8a1e127891264ce
marcelo <fleitascampeloml@gmail.com>
2018-05-30 16:58:55 -0300

    Se crearon las migraciones

---
+++ resources/views/proveedores/listado.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+    <html lang="es">
+        <head>
+            <meta charset="utf-8" />
+            <link rel= "stylesheet" href= "https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity= "sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin= "anonymous" > 
+            <title>Mi primer Listado!</title>
+        </head>
+        <body>
+            <h1>LISTADO!</h1>
+            <!– Para que el sitio sea RESPONSIVE o sea se adapte a todos los dispositivos.
+            <div class="container">
+                <table class="table">
+                    <thead>
+                        <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        </tr>
+                        <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                        </tr>
+                        <tr>
+                        <th scope="row">3</th>
+                        <td>Larry</td>
+                        <td>the Bird</td>
+                        <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                    </table>
+            </div>
+        </body>
+    </html>

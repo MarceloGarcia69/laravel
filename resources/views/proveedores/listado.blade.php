2172b99e0b37166dd3bdc314d1de4fe405ee5356
Marcelo Garcia <marcelo69@gmail.com>
2018-05-31 18:33:36 -0300

    Actualizando cambios marceloG

---
+++ resources/views/proveedores/listado.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+    <html lang="es">
+        <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+        
+            <meta charset="utf-8" />
+            <title>Hola Mundo!</title>
+        </head>
+        <body>
+            <h1>Hola Mundo!</h1>
+
+            <div class"contaier">
+
+              <table class="table">
+                <thead>
+                    <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">3</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@twitter</td>
+                    </tr>
+                </tbody>
+              </table>
+
+
+
+
+
+            </div> 
+
+
+
+        </body>
+    </html>
